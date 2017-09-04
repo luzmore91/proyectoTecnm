00ef89c132132887fdefdea83889a25185dcd588
Ing. Armando <Arrova931026@Ings-MacBook-Pro.local>
2017-09-04 12:31:39 -0500

    cambiosNando

---
+++ resources/views/recursosAplicacion.blade.php
@@ -0,0 +1,86 @@
+
+<div>
+
+<table class="table table-condensed table-striped">
+
+  <tr>
+    <th>
+    	<b>Recursos; </b><i>Indique los recursos humanos, tecnológicos y financieros con los que cuenta actualmente para que la tecnología/proyecto avance.</i>
+   	</th>
+  </tr>
+
+  <tr>
+    <td>
+		<textarea class="form-control" rows="3" placeholder="RECURSOS HUMANOS:"></textarea>
+    </td>
+
+  </tr>
+  <tr>
+    <td>
+		<textarea class="form-control" rows="3" placeholder="RECURSOS TECNOLÓGICOS:"></textarea>
+    </td>
+    
+  </tr>
+     <tr>
+    <td>
+		<textarea class="form-control" rows="3" placeholder="RECURSOS FINANCIEROS:"></textarea>
+    </td>
+    
+  </tr>
+
+</table>
+
+<table class="table table-condensed table-striped">
+
+  <tr>
+    <th>
+    	<b>Usos/Aplicaciones; </b><i>Indique el/los mercado(s) a los que va dirigida su tecnología y el/los uso(s) que se darán en éste(os).</i>
+   	</th>
+  </tr>
+
+  <tr>
+    <td>
+		<textarea class="form-control" rows="2"></textarea>
+    </td>
+
+  </tr>
+  
+</table>
+
+<table class="table table-condensed table-striped">
+
+  <tr>
+    <th>
+      <b>Viabilidad; </b><i>Indique si para la la tecnología/proyecto se ha validado la viabilidad técnica y financiera, y la existencia de prospectos interesados en la misma.</i>
+    </th>
+  </tr>
+
+  <tr>
+    <td>
+    <textarea class="form-control" rows="2"></textarea>
+    </td>
+
+  </tr>
+  
+</table>
+
+<table class="table table-condensed table-striped">
+
+  <tr>
+    <th>
+      <b>Beneficios; </b><i>Para el proyecto/tecnología indique la cuantificación de los beneficios en términos de rendimiento, físicos, costos, ambientales y ciclo de vida del producto de acuerdo al mercado identificado.</i>
+    </th>
+  </tr>
+
+  <tr>
+    <td>
+    <textarea class="form-control" rows="2"></textarea>
+    </td>
+
+  </tr>
+  
+</table>
+
+<br>
+
+</div>
