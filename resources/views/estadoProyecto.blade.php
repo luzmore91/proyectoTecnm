b61b3447a44cd52049b84e47ad3020b00319e498
luzmore91 <luz.moreno17ce@cenidet.edu.mx>
2017-09-04 11:36:57 -0500

    cambios en parte del formulario - luz

--- resources/views/estadoProyecto.blade.php
+++ resources/views/estadoProyecto.blade.php
@@ -1,6 +1,63 @@
-<div>
+<div class="row" style="padding-top:20px" >
+<div class="col-xs-12" style="padding-top:20px">
+<div class="col-xs-5"><label>2. Estado de desarrollo/madurez del proyecto/tecnología (TRL)</label></div>
+<div class="col-xs-7">
+ 	<select class="form-control selectpicker" data-style="btn-green">
+                  		<option>Seleccione una opción</option>
+                  		<option>Opción 01</option>
+                  		<option>Opción 02</option>
+                  		<option>Opción 03</option>
+                	</select>
+</div>
+</div>
+<div class="col-xs-12" style="padding-top:20px">
+<div class="col-xs-5"><label>3. Sector estratégico</label></div>
+<div class="col-xs-7">
+ 	<select class="form-control selectpicker" data-style="btn-green">
+                  		<option>Seleccione una opción</option>
+                  		<option>Opción 01</option>
+                  		<option>Opción 02</option>
+                  		<option>Opción 03</option>
+                	</select>
+</div>
+</div>
+<div class="col-xs-12" style="padding-top:20px">
+<div class="col-xs-3"><label>4. Propiedad Intelectual.</label> <label style="font-style:italic"> Estado actual</label></div>
+<div class="col-xs-3">
+ 	<select class="form-control selectpicker" data-style="btn-green">
+                  		<option>Seleccione una opción</option>
+                  		<option>Opción 01</option>
+                  		<option>Opción 02</option>
+                  		<option>Opción 03</option>
+                	</select>
+</div>
 
-<label>2. Estado de desarrollo/madurez del proyecto/tecnología (TRL)</label><br/>
-  <button type="button" class="btn dropdown-toggle selectpicker btn-green" data-toggle="dropdown" title="Opción 03"><span class="filter-option pull-left">Opción 03</span>&nbsp;<span class="caret"></span></button>
+        <div class="col-xs-3"><label>En caso de que esté en proceso, revisión y otorgada la protección</label> <label style="font-style:italic">  favor de indicar el tipo de protección</label></div>
+<div class="col-xs-3">
+ 	<select class="form-control selectpicker" data-style="btn-green">
+                  		<option>Seleccione una opción</option>
+                  		<option>Opción 01</option>
+                  		<option>Opción 02</option>
+                  		<option>Opción 03</option>
+                	</select>
+</div>
 
+</div>
+<div class="col-xs-12" style="padding-top:20px">
+           <div class="col-xs-3"><label>5. Lo que persigue el proyecto/tecnología es</label></div>
+<div class="col-xs-3">
+ 	<select class="form-control selectpicker" data-style="btn-green">
+                  		<option>Seleccione una opción</option>
+                  		<option>Opción 01</option>
+                  		<option>Opción 02</option>
+                  		<option>Opción 03</option>
+                	</select>
+</div>
+
+        <div class="col-xs-3"><label>En caso de seleccionar la opción "Otro", describir en el recuadro anexo.</label> </div>
+<div class="col-xs-3">
+ 	 <input class="form-control" type="text" name="otro_ObjetivoProyecto"/>
+</div>
+
+</div>
 </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,16 @@
 <html>
     <head>
         <title>Tecnologico de mexico</title>
-    <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('/css/bootstrap-timepicker.min.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('/css/dataTables.bootstrap.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/dataTables.bootstrap.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('/css/bootstrap-select.css') }}" rel="stylesheet" type="text/css">
     <!-- Custom CSS -->
-    <link href="{{ asset('/css/form.css') }}" rel="stylesheet">
-         <link href="{{ asset('/css/format.css') }}" rel="stylesheet">
-         <link href="{{ asset('/css/ie.css') }}" rel="stylesheet">
-        <link href="{{ asset('/css/styles.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/form.css') }}" rel="stylesheet" type="text/css">
+         <link href="{{ asset('/css/format.css') }}" rel="stylesheet" type="text/css">
+         <link href="{{ asset('/css/ie.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('/css/styles.css') }}" rel="stylesheet" type="text/css">
     </head>
     <body>
         <div class="container">
