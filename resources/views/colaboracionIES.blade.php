ef310076332dd1cd4341935aebcdb6d5274cf4b9
Mercedes Miranda <mervic1706@gmail.com>
2017-09-04 17:42:59 -0500

    archivo terminado del 6 al 8

---
+++ resources/views/colaboracionIES.blade.php
@@ -0,0 +1,97 @@
+
+<h2>6. Colaboración con otras IES. Describa las IES y el tipo de colaboración que el proyecto tiene con éstas.</h2>
+<textarea class="form-control" rows="6"></textarea>
+             
+
+<h2>7. Riesgos. Describa los riesgos asociados con su proyecto/tecnología, y sus estrategias de mitigación.</h2>
+
+<table id="altaEquipo" class="table table-striped table-hover sortable">
+                        <thead>
+                            <tr>
+                                <th><center>Tipo de riesgo</center></th>
+                                <th><center>Descripción</center></th>
+                                <th><center>Estrategia de mitigación</center></th>                             
+                            </tr>
+                        </thead>
+             
+                        <tbody>
+                             <tr>
+                                <td class="text-center"> <select id="estudios1" class="form-control selectpicker" data-style="btn-green">
+                  		          <option>Seleccione una opción</option>
+                  		          <option>Tecnológico</option>
+                  		          <option>Operativo</option>
+                  		          <option>Legal</option>
+                                  <option>Ecológico</option>
+                                  <option>Social</option>
+                                  <option>De negocio</option>
+                                  <option>Otro</option>
+                	            </select></td>
+                <td><input type="text" class="form-control" id="Decripcion1" placeholder=""/></td>
+                <td><input type="text" class="form-control" id="Estrategia1" placeholder=""/></td>
+                   
+        
+                       
+                             <tr>
+                                <td class="text-center"> <select id="estudios1" class="form-control selectpicker" data-style="btn-green">
+                  		          <option>Seleccione una opción</option>
+                  		          <option>Tecnológico</option>
+                  		          <option>Operativo</option>
+                  		          <option>Legal</option>
+                                  <option>Ecológico</option>
+                                  <option>Social</option>
+                                  <option>De negocio</option>
+                                  <option>Otro</option>
+                	            </select></td>
+                <td><input type="text" class="form-control" id="Decripcion2" placeholder=""/></td>
+                <td><input type="text" class="form-control" id="Estrategia2" placeholder=""/></td>
+            
+                       
+                             <tr>
+                                <td class="text-center"> <select id="estudios1" class="form-control selectpicker" data-style="btn-green">
+                  		          <option>Seleccione una opción</option>
+                  		          <option>Tecnológico</option>
+                  		          <option>Operativo</option>
+                  		          <option>Legal</option>
+                                  <option>Ecológico</option>
+                                  <option>Social</option>
+                                  <option>De negocio</option>
+                                  <option>Otro</option>
+                	            </select></td>
+                <td><input type="text" class="form-control" id="Decripcion3" placeholder=""/></td>
+                <td><input type="text" class="form-control" id="Estrategia3" placeholder=""/></td>
+                                 
+                  
+                             <tr>
+                                <td class="text-center"> <select id="estudios1" class="form-control selectpicker" data-style="btn-green">
+                  		          <option>Seleccione una opción</option>
+                  		          <option>Tecnológico</option>
+                  		          <option>Operativo</option>
+                  		          <option>Legal</option>
+                                  <option>Ecológico</option>
+                                  <option>Social</option>
+                                  <option>De negocio</option>
+                                  <option>Otro</option>
+                	            </select></td>
+                <td><input type="text" class="form-control" id="Decripcion4" placeholder=""/></td>
+                <td><input type="text" class="form-control" id="Estrategia4" placeholder=""/></td>
+            
+                        
+                             <tr>
+                                <td class="text-center"> <select id="estudios1" class="form-control selectpicker" data-style="btn-green">
+                  		          <option>Seleccione una opción</option>
+                  		          <option>Tecnológico</option>
+                  		          <option>Operativo</option>
+                  		          <option>Legal</option>
+                                  <option>Ecológico</option>
+                                  <option>Social</option>
+                                  <option>De negocio</option>
+                                  <option>Otro</option>
+                	            </select></td>
+                <td><input type="text" class="form-control" id="Decripcion5" placeholder=""/></td>
+                <td><input type="text" class="form-control" id="Estrategia5" placeholder=""/></td>
+            </tr> 
+            </tbody>
+</table>                  
+      
+<h2>8. Análisis del entorno. Indique el tamaño de la industria a la que va dirigida su tecnología/proyecto, la industria que le proveería sus insumos, clientes/consumidores de su tecnología/proyecto, las tecnologías/proyectos competidores y sus ofertas, y sus factores de demanda.</h2>
+<textarea class="form-control" rows="6"></textarea>
