ae8cc1d7f862d8257451ebcaaba6c3f924f4c3f5
luzmore91 <luz.moreno17ce@cenidet.edu.mx>
2017-09-04 10:24:16 -0500

    include php blade

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,3 +1,4 @@
+
 <!-- Footer starts -->
 	<div id="footer">
     	<div class="row">
---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>Tecnologico de mexico</title>
+    <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('/css/bootstrap-timepicker.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('/css/dataTables.bootstrap.css') }}" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="{{ asset('/css/form.css') }}" rel="stylesheet">
+         <link href="{{ asset('/css/format.css') }}" rel="stylesheet">
+         <link href="{{ asset('/css/ie.css') }}" rel="stylesheet">
+        <link href="{{ asset('/css/styles.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            @include('header')
+        </div>
+
+
+
+        <div class="container">
+            @include('footer')
+        </div>
+    </body>
+</html>
