06ddbe3781bad585c44e61ff939fb61935df1918
luzmore91 <luz.moreno17ce@cenidet.edu.mx>
2017-09-04 16:23:28 -0500

    cambios de yisus

--- resources/views/recursosAplicacion.blade.php
+++ resources/views/recursosAplicacion.blade.php
@@ -1,15 +1,9 @@
 
-<div>
+<div class="row">
+ <h2>9. Recursos</h2>
+    	<label style="font-style:italic">Indique los recursos humanos, tecnológicos y financieros con los que cuenta actualmente para que la tecnología/proyecto avance.</label>
 
 <table class="table table-condensed table-striped">
-
-  <tr>
-    <th>
-        <h2>9. Recursos</h2>
-    	<label style="font-style:italic">Indique los recursos humanos, tecnológicos y financieros con los que cuenta actualmente para que la tecnología/proyecto avance.</label>
-   	</th>
-  </tr>
-
   <tr>
     <td>
 		<textarea class="form-control" rows="3" placeholder="RECURSOS HUMANOS:"></textarea>
@@ -30,16 +24,10 @@
   </tr>
 
 </table>
-
-<table class="table table-condensed table-striped">
-
-  <tr>
-    <th>
-         <h2>10.Usos/Aplicaciones</h2>
+<h2>10.Usos/Aplicaciones</h2>
     	<label style="font-style:italic">Indique el/los mercado(s) a los que va dirigida su tecnología y el/los uso(s) que se darán en éste(os).</label>
 
-   	</th>
-  </tr>
+<table class="table table-condensed table-striped">
 
   <tr>
     <td>
@@ -49,16 +37,10 @@
   </tr>
   
 </table>
-
-<table class="table table-condensed table-striped">
-
-  <tr>
-    <th>
-         <h2>11.Viabilidad</h2>
+ <h2>11.Viabilidad</h2>
     	<label style="font-style:italic">Indique si para la la tecnología/proyecto se ha validado la viabilidad técnica y financiera, y la existencia de prospectos interesados en la misma.</label>
 
-    </th>
-  </tr>
+<table class="table table-condensed table-striped">
 
   <tr>
     <td>
@@ -68,19 +50,11 @@
   </tr>
   
 </table>
-
-<table class="table table-condensed table-striped">
-
-  <tr>
-    <th>
-
-         <h2>12.Beneficios</h2>
+  <h2>12.Beneficios</h2>
     	<label style="font-style:italic">Para el proyecto/tecnología indique la cuantificación de los beneficios en términos de rendimiento, físicos, costos, ambientales y ciclo de vida del producto de acuerdo al mercado identificado.</label>
 
 
-    </th>
-  </tr>
-
+<table class="table table-condensed table-striped">
   <tr>
     <td>
     <textarea class="form-control" rows="2"></textarea>
