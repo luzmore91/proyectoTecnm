8e65ea534b9bab37c662d88276f0721069bb124a
Mercedes Miranda <mervic1706@gmail.com>
2017-09-06 12:39:36 -0500

    hola cambios meche

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,14 @@
 <html>
     <head>
         <title>Tecnologico de mexico</title>
-    <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('/css/app.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('/css/bootstrap-datetimepicker.min.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('/css/bootstrap-select.css') }}" rel="stylesheet" type="text/css">
-    <!-- Custom CSS -->
-    <link href="{{ asset('/css/form.css') }}" rel="stylesheet" type="text/css">
-         <link href="{{ asset('/css/format.css') }}" rel="stylesheet" type="text/css">
-         <link href="{{ asset('/css/ie.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('/css/bootstrap-datetimepicker.min.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('/css/bootstrap-select.css') }}" rel="stylesheet" type="text/css">
+        <!-- Custom CSS -->
+        <link href="{{ asset('/css/form.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('/css/format.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('/css/ie.css') }}" rel="stylesheet" type="text/css">
         <link href="{{ asset('/css/styles.css') }}" rel="stylesheet" type="text/css">
     </head>
     <body>
@@ -16,37 +16,36 @@
             @include('header')
         </div>
 
-        <!--Tecnologia o Proyecto y Equipo Emprendedor o Inventor-->
-        <div class="container">
-            @include('tecnologiaEquipoEmprendedor')
-        </div>
-        <!--Estado de proyecto, sector estrategico , propiedad intelectual y lo que persigue el proyecto-->
-        <div class="container">
-            @include('estadoProyecto')
-        </div>
-         <div class="container">
-            @include('colaboracionIES')
-        </div>
-        <div class="container">
-            @include('recursosAplicacion')
-        </div>
-         <div class="container" style="padding-bottom:60px">
-
-         <div class="row col-xs-12" id="botones">
-         <div class="col-xs-6">
-            <a href="#" class="btn btn-success btn-lg" style="    left: 80%;
-    position: relative;">
-      <span class="glyphicon glyphicon-ok"></span> Aceptar
-    </a>
+        <form name="formRegistro" method="post" action="">
+            <!--Tecnologia o Proyecto y Equipo Emprendedor o Inventor-->
+            <div class="container">
+                @include('tecnologiaEquipoEmprendedor')
             </div>
-            <div class="col-xs-6">
-            <a href="#" class="btn btn-danger btn-lg">
-      <span class="glyphicon glyphicon-trash"></span> Cancelar
-    </a>
+            <!--Estado de proyecto, sector estrategico , propiedad intelectual y lo que persigue el proyecto-->
+            <div class="container">
+                @include('estadoProyecto')
+            </div>
+            <div class="container">
+                @include('colaboracionIES')
+            </div>
+            <div class="container">
+                @include('recursosAplicacion')
+            </div>
+            <div class="container" style="padding-bottom:60px">
+
+                <div class="row col-xs-12" id="botones">
+                    <div class="col-xs-6">
+                        <button href="#" class="btn btn-success btn-lg" style="left: 80%;position: relative;">
+                            <span class="glyphicon glyphicon-ok">Aceptar</span>
+                        </button>
+                    </div>
+                    <div class="col-xs-6">
+                        <button href="#" class="btn btn-danger btn-lg"><span class="glyphicon glyphicon-trash">Cancelar</span></button>
+                    </div>
+                </div>
             </div>
 
-        </div>
-        </div>
+        </form>
 
         <div class="container">
             @include('footer')
@@ -55,8 +54,9 @@
 
     <!-- Select -->
     <script type="text/javascript" src="{{asset('/js/jquery.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('/js/bootstrap.js')}}"></script>
-<script type="text/javascript" src="{{ asset('/js/bootstrap-select.js') }}"></script>
-<script type="text/javascript" src="{{ asset('/js/codigoSelect.js') }}"></script>
-
+    <script type="text/javascript" src="{{asset('/js/bootstrap.js')}}"></script>
+    <script type="text/javascript" src="{{ asset('/js/bootstrap-select.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/codigoSelect.js') }}"></script>
+    <script>
+    </script>
 </html>
