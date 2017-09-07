5d99f21bd5ca6bde26ef50bcc6347e0e4f324af7
Mercedes Miranda <mervic1706@gmail.com>
2017-09-07 09:58:43 -0500

    cambios meche

--- resources/views/colaboracionIES.blade.php
+++ resources/views/colaboracionIES.blade.php
@@ -10,7 +10,7 @@
 
 <label style="font-style:italic">Describa las IES y el tipo de colaboración que el proyecto tiene con éstas.</label>
 
-<textarea class="form-control" rows="6" placeholder= "Descripción de Institución de Educación Superior
+<textarea style="resize: vertical" class="form-control" rows="6" placeholder= "Descripción de Institución de Educación Superior
 " title="Descripción IES y tipo de colaboración" required></textarea>
              
 <div class="col-xs-12 espacioTitulosContenido">
@@ -120,6 +120,6 @@
            </button></div>
 </div>
 <label style="font-style:italic">Indique el tamaño de la industria a la que va dirigida su tecnología/proyecto, la industria que le proveería sus insumos, clientes/consumidores de su tecnología/proyecto, las tecnologías/proyectos competidores y sus ofertas, y sus factores de demanda.</label>
-<textarea class="form-control" rows="6" placeholder= "Indique el tamaño de la industria a la que va dirigida su tecnología
+<textarea style="resize: vertical" class="form-control" rows="6" placeholder= "Indique el tamaño de la industria a la que va dirigida su tecnología
 " title="Indique el tamaño de la industria" required></textarea>
 </div>
