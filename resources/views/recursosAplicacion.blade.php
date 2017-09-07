8aa08ef73e08b151a6d68e6fb92dc73e21c3e9fd
Ing. Armando <Arrova931026@Ings-MacBook-Pro.local>
2017-09-06 23:28:26 -0500

    cambios

--- resources/views/recursosAplicacion.blade.php
+++ resources/views/recursosAplicacion.blade.php
@@ -4,14 +4,10 @@ textarea {
 }
 </style>
 <div class="row">
- <h2>9. Recursos 
- 
- <button type="button" class="btn btn-green popAyuda" title="" data-container="body" data-toggle="popover" data-placement="right" data-content="En esta sección se debe realizar una descripción de los recursos; 'Humanos, Tecnológicos y Financieros', utilizar en todo momento una redacción clara, concisa y completamente entendible.  " data-original-title="Recursos.">
-  <span class="glyphicon glyphicon-question-sign"></span>
-  </button>
- 
-  </h2>
+ <h2>9. Recursos</h2>
+
     	<label style="font-style:italic">Indique los recursos humanos, tecnológicos y financieros con los que cuenta actualmente para que la tecnología/proyecto avance.</label>
+
 
 <table class="table table-condensed table-striped">
   <tr>
@@ -35,9 +31,7 @@ textarea {
 
 </table>
 <h2>10.Usos/Aplicaciones 
-<button type="button" class="btn btn-green popAyuda" title="" data-container="body" data-toggle="popover" data-placement="right" data-content="Aqui se debe describir el tipo de público (usualmente personas) a cual va dirigido el proyecto/tecnología, y además indicar para que va a ser útil.  " data-original-title="Usos y/o Aplicaciones.">
-<span class="glyphicon glyphicon-question-sign"></span>
-</button>
+
 </h2>
 <label style="font-style:italic">Indique el/los mercado(s) a los que va dirigida su tecnología y el/los uso(s) que se darán en éste(os).</label>
 
@@ -52,9 +46,7 @@ textarea {
   
 </table>
  <h2>11.Viabilidad
-<button type="button" class="btn btn-green popAyuda" title="" data-container="body" data-toggle="popover" data-placement="right" data-content="Descripción de la factibilidad de la realización de este proyecto desde los puntos de vista técnico y financiero, además si ya existen partes interesadas en este proyecto también indicarlo (Empresas, Empresarios, etc...) " data-original-title="Viabilidad.">
-<span class="glyphicon glyphicon-question-sign"></span>
-</button>
+
  </h2>
     	<label style="font-style:italic">Indique si para la la tecnología/proyecto se ha validado la viabilidad técnica y financiera, y la existencia de prospectos interesados en la misma.</label>
 
@@ -68,11 +60,7 @@ textarea {
   </tr>
   
 </table>
-  <h2>12.Beneficios
-<button type="button" class="btn btn-green popAyuda" title="" data-container="body" data-toggle="popover" data-placement="right" data-content="Para el proyecto/tecnología indique la cuantificación de los beneficios en términos de rendimiento, físicos, costos, ambientales y ciclo de vida del producto de acuerdo al mercado identificado." data-original-title="Beneficios.">
-<span class="glyphicon glyphicon-question-sign"></span>
-</button>
-  </h2>
+  <h2>12.Beneficios</h2>
     	<label style="font-style:italic">Para el proyecto/tecnología indique la cuantificación de los beneficios en términos de rendimiento, físicos, costos, ambientales y ciclo de vida del producto de acuerdo al mercado identificado.</label>
 
 <table class="table table-condensed table-striped">
