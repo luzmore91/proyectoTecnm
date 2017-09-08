7e63290dcd0bb28301f90f8e398a5c5919f8ea01
Mercedes Miranda <mervic1706@gmail.com>
2017-09-08 00:08:36 -0500

    cambiosRa

--- resources/views/recursosAplicacion.blade.php
+++ resources/views/recursosAplicacion.blade.php
@@ -1,7 +1,7 @@
 
 <div class="row">
     
-<div class="col-xs-12 espacioTitulosContenido">    
+<div class="col-xs-13 espacioTitulosContenido">    
     <div class="col-xs-3"> <h2>9. Recursos.</h2></div>
     <div class="col-xs-1">
         <button type="button" class="btn btn-green popAyuda" title="" data-container="body" data-toggle="popover" data-placement="right" data-content="En esta sección se debe realizar una descripción de los recursos; 'Humanos, Tecnológicos y Financieros', utilizar en todo momento una redacción clara, concisa y completamente entendible.  " data-original-title="Recursos.">
@@ -9,7 +9,7 @@
         </button>
     </div>
 </div>
-    
+    <div class="col-xs-12">
     	<label style="font-style:italic">Indique los recursos humanos, tecnológicos y financieros con los que cuenta actualmente para que la tecnología/proyecto avance.</label>
 
 <table class="table table-condensed table-striped">
@@ -32,14 +32,14 @@
     
   </tr>
 
-</table>
-<div class="col-xs-12 espacioTitulosContenido">
+</table></div>
+<div class="col-xs-13 espacioTitulosContenido">
     <div class="col-xs-3"><h2>10.Usos/Aplicaciones</h2></div>
     <div class="col-xs-1"><button type="button" class="btn btn-green popAyuda" title="" data-container="body" data-toggle="popover" data-placement="right" data-content="Aqui se debe describir el tipo de público (usualmente personas) a cual va dirigido el proyecto/tecnología, y además indicar para que va a ser útil.  " data-original-title="Usos y/o Aplicaciones.">
         <span class="glyphicon glyphicon-question-sign"></span>
     </button></div>
 </div>    
-
+<div class="col-xs-12">
 <label style="font-style:italic">Indique el/los mercado(s) a los que va dirigida su tecnología y el/los uso(s) que se darán en éste(os).</label>
 
 <table class="table table-condensed table-striped">
@@ -51,14 +51,14 @@
 
   </tr>
   
-</table>
-    <div class="col-xs-12 espacioTitulosContenido"> 
+</table></div>
+    <div class="col-xs-13 espacioTitulosContenido"> 
         <div class="col-xs-2"><h2>11.Viabilidad</h2></div> 
         <div class="col-xs-1"><button type="button" class="btn btn-green popAyuda" title="" data-container="body" data-toggle="popover" data-placement="right" data-content="Descripción de la factibilidad de la realización de este proyecto desde los puntos de vista técnico y financiero, además si ya existen partes interesadas en este proyecto también indicarlo (Empresas, Empresarios, etc...) " data-original-title="Viabilidad."> 
             <span class="glyphicon glyphicon-question-sign"></span>
             </button></div>
     </div>
-        
+        <div class="col-xs-12">
     	<label style="font-style:italic">Indique si para la la tecnología/proyecto se ha validado la viabilidad técnica y financiera, y la existencia de prospectos interesados en la misma.</label>
 
 <table class="table table-condensed table-striped">
@@ -70,13 +70,14 @@
 
   </tr>
   
-</table>
-    <div class="col-xs-12 espacioTitulosContenido">
+</table></div>
+    <div class="col-xs-13 espacioTitulosContenido">
         <div class="col-xs-2"> <h2>12.Beneficios  </h2> </div>
         <div class="col-xs-1"> <button type="button" class="btn btn-green popAyuda" title="" data-container="body" data-toggle="popover" data-placement="right" data-content="Para el proyecto/tecnología indique la cuantificación de los beneficios en términos de rendimiento, físicos, costos, ambientales y ciclo de vida del producto de acuerdo al mercado identificado." data-original-title="Beneficios.">
             <span class="glyphicon glyphicon-question-sign"></span>
         </button></div>
         
+        <div class="col-xs-12">
     	<label style="font-style:italic">Para el proyecto/tecnología indique la cuantificación de los beneficios en términos de rendimiento, físicos, costos, ambientales y ciclo de vida del producto de acuerdo al mercado identificado.</label>
 
 <table class="table table-condensed table-striped">
@@ -90,4 +91,4 @@
 </table>
 <br>
 
-</div>
+</div></div>
