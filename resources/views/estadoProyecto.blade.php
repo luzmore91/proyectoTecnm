2e41044af52cd29defb5b27c975bcf5919f635e2
Luis Lopez <lalg.2191@gmail.com>
2017-09-08 16:53:41 -0500

    ultimos selecs(propiedad intelectual tipoproteccion)

--- resources/views/estadoProyecto.blade.php
+++ resources/views/estadoProyecto.blade.php
@@ -54,21 +54,19 @@
 <div class="col-xs-2"><label style="font-style:italic"> Estado actual</label></div>
 <div class="col-xs-3">
  	<select class="form-control selectpicker" data-style="btn-green">
-                  		<option>Seleccione una opción</option>
-                  		<option>Opción 01</option>
-                  		<option>Opción 02</option>
-                  		<option>Opción 03</option>
-                	</select>
+        @foreach ($propInt as $itera)
+        <option> {{ $itera->descripcion }}</option>
+        @endforeach
+    </select>
 </div>
 
         <div class="col-xs-3"><label>En caso de que esté en proceso, revisión y otorgada la protección</label> <label style="font-style:italic">  favor de indicar el tipo de protección</label></div>
 <div class="col-xs-4">
- 	<select class="form-control selectpicker" data-style="btn-green">
-                  		<option>Seleccione una opción</option>
-                  		<option>Opción 01</option>
-                  		<option>Opción 02</option>
-                  		<option>Opción 03</option>
-                	</select>
+    <select class="form-control selectpicker" data-style="btn-green">
+        @foreach ($prot as $itera)
+        <option> {{ $itera->descripcion }}</option>
+        @endforeach
+    </select>
 </div>
 
 </div>
