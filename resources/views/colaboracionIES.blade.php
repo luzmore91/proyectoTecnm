94cdfb0500b0283dbcd80979795cdc1a0acde13e
Mercedes Miranda <mervic1706@gmail.com>
2017-09-07 22:58:10 -0500

    cambios IES

--- resources/views/colaboracionIES.blade.php
+++ resources/views/colaboracionIES.blade.php
@@ -1,5 +1,5 @@
-<div class="row" >
-<div class="col-xs-12 espacioTitulosContenido">
+<div class="row">
+<div class="col-xs-13 espacioTitulosContenido">
 <div class="col-xs-4"><h2>6. Colaboración con otras IES.</h2></div>
 <div class="col-xs-1"><button type="button" class="btn btn-green popAyuda" title="Ayuda"
               data-container="body" data-toggle="popover" data-placement="right"
@@ -7,13 +7,14 @@
               <span class="glyphicon glyphicon-question-sign"></span>
            </button></div>
 </div>
+<div class="col-xs-12">
+<label style="font-style:italic">Describa las IES y el tipo de colaboración que el proyecto tiene con éstas.</label> </div>
 
-<label style="font-style:italic">Describa las IES y el tipo de colaboración que el proyecto tiene con éstas.</label>
-
+<div class="col-xs-12">
 <textarea style="resize: vertical" class="form-control" rows="6" placeholder= "Descripción de Institución de Educación Superior
-" title="Descripción IES y tipo de colaboración" required></textarea>
+" title="Descripción IES y tipo de colaboración" required></textarea></div>
              
-<div class="col-xs-12 espacioTitulosContenido">
+<div class="col-xs-13 espacioTitulosContenido">
 <div class="col-xs-2"><h2>7. Riesgos.</h2></div>
 <div class="col-xs-1"><button type="button" class="btn btn-green popAyuda" title="Ayuda"
               data-container="body" data-toggle="popover" data-placement="right"
@@ -21,8 +22,10 @@
               <span class="glyphicon glyphicon-question-sign"></span>
            </button></div>
 </div>
-<label style="font-style:italic">Describa los riesgos asociados con su proyecto/tecnología, y sus estrategias de mitigación.</label>
+<div class="col-xs-12">
+<label style="font-style:italic">Describa los riesgos asociados con su proyecto/tecnología, y sus estrategias de mitigación.</label></div>
 
+<div class="col-xs-12">
 <table id="altaEquipo" class="table table-striped table-hover sortable">
                         <thead>
                             <tr>
@@ -109,9 +112,9 @@
                 <td><textarea class="form-control" id="Estrategia5" placeholder="Estrategia" style="resize: none" title="Se requiere una estrategia" required="required" ></textarea></td>
             </tr> 
             </tbody>
-</table>                  
+</table></div>
 
-<div class="col-xs-12 espacioTitulosContenido">
+<div class="col-xs-13 espacioTitulosContenido">
 <div class="col-xs-3"><h2>8. Análisis del entorno.</h2></div>
 <div class="col-xs-1"><button type="button" class="btn btn-green popAyuda" title="Ayuda"
               data-container="body" data-toggle="popover" data-placement="right"
@@ -119,7 +122,9 @@
               <span class="glyphicon glyphicon-question-sign"></span>
            </button></div>
 </div>
+
+<div class="col-xs-12">
 <label style="font-style:italic">Indique el tamaño de la industria a la que va dirigida su tecnología/proyecto, la industria que le proveería sus insumos, clientes/consumidores de su tecnología/proyecto, las tecnologías/proyectos competidores y sus ofertas, y sus factores de demanda.</label>
 <textarea style="resize: vertical" class="form-control" rows="6" placeholder= "Indique el tamaño de la industria a la que va dirigida su tecnología
 " title="Indique el tamaño de la industria" required></textarea>
-</div>
+</div></div>
